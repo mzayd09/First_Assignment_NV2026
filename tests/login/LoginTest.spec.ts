35dd79657075c011b467e040f2b5a53aef9a7303
Mohd Zayd <mohd.zayd@newvision-software.com>
2026-04-09 16:49:32 +0530

    fixture-use in specfile

--- tests/login/LoginTest.spec.ts
+++ tests/login/LoginTest.spec.ts
@@ -1,6 +1,7 @@
-import {test, expect} from "@playwright/test";
-import { LoginAction } from "../../src/action/LoginAction.ts";
+//import {test, expect} from "@playwright/test";//
+import {test, expect} from "../../src/fixture/fixture";
 
+//import { LoginAction } from "../../src/action/LoginAction.ts";
 import loginData from "../../src/testdata/Logintestdata.json";
 
 
@@ -9,22 +10,22 @@ test.beforeEach(async ({page}) => {
 });
 
 
-test('TC-01: Validate user should login successfully', async ({page}) =>{
-const loginAction = new LoginAction(page);
+test('TC-01: Validate user should login successfully', async ({loginAction,page}) =>{
+//const loginAction = new LoginAction(page);
 await loginAction.login(loginData.ValidUser.username, loginData.ValidUser.password);
 await expect(page).toHaveTitle(loginData.PageTitle);
 await expect(page).toHaveURL(loginData.DashboardUrl);
 });
 
-test('TC-02: Locked out user should not login', async ({page}) =>{
-const loginAction = new LoginAction(page);
+test('TC-02: Locked out user should not login', async ({loginAction}) =>{
+//const loginAction = new LoginAction(page);
 const errormsg = await loginAction.getErrorMessage();
 await loginAction.login(loginData.lockedUser.username, loginData.lockedUser.password);
 await expect(errormsg).toHaveText(loginData.lockedUser.errorMessage);   
 });
 
-test('TC-03: invalid user should not login', async ({page}) =>{
-const loginAction = new LoginAction(page);
+test('TC-03: invalid user should not login', async ({loginAction}) =>{
+//const loginAction = new LoginAction(page);
 const errormsg = await loginAction.getErrorMessage();
 await loginAction.login(loginData.invalidUser.username, loginData.invalidUser.password);
 await expect(errormsg).toHaveText(loginData.invalidUser.errorMessage);   
